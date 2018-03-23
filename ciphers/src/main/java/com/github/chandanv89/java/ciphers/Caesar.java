997de62cdff46faffe6788d40646bef84f5435bc
Chandan <chandanv89@gmail.com>
2018-03-23 15:18:26 +0000

    Code cleanup for com.github.chandanv89.java.ciphers

--- ciphers/src/main/java/com/github/chandanv89/java/ciphers/Caesar.java
+++ ciphers/src/main/java/com/github/chandanv89/java/ciphers/Caesar.java
@@ -8,97 +8,81 @@ package com.github.chandanv89.java.ciphers;
  * @author : FAHRI YARDIMCI
  */
 public class Caesar {
-    /**
-     * Encode string.
-     *
-     * @param message the message
-     * @param shift   the shift
-     * @return the string
-     */
-    public static String encode(String message, int shift) {
-        StringBuilder encoded = new StringBuilder();
-        for (int i = 0; i < message.length(); i++) {
-            int current = message.charAt(i); //using char to shift characters because ascii is in-order latin alphabet
-            if (current == 32) {
-                encoded.append(" ");
-            } else if (current >= 65 && current <= 90) {
-                int numAlphabet = message.charAt(i);
-                if (shift + numAlphabet > 90) {
-                    int j = 90 - numAlphabet;
-                    char nextKey = (char) (65 + (shift - j - 1));
-                    encoded.append(nextKey);
+   /**
+    * Encode string.
+    *
+    * @param message the message
+    * @param shift   the shift
+    * @return the string
+    */
+   public static String encode(String message, int shift) {
+      StringBuilder encoded = new StringBuilder();
+      for (int i = 0; i < message.length(); i++) {
+         int current = message.charAt(i); //using char to shift characters because ascii is in-order latin alphabet
+         if (current == 32) {
+            encoded.append(" ");
+         } else if (current >= 65 && current <= 90) {
+            int numAlphabet = message.charAt(i);
+            if (shift + numAlphabet > 90) {
+               int j = 90 - numAlphabet;
+               char nextKey = (char) (65 + (shift - j - 1));
+               encoded.append(nextKey);
 
-                } else {
-                    char nextKey = (char) (current + shift);
-                    encoded.append(nextKey);
-                }
-            } else if (current >= 97 && current <= 122) {
-                int numAlphabet = message.charAt(i);
-                if (shift + numAlphabet > 122) {
-                    int j = 122 - numAlphabet;
-                    char nextKey = (char) (97 + (shift - j - 1));
-                    encoded.append(nextKey);
-                } else {
-                    char nextKey = (char) (current + shift);
-                    encoded.append(nextKey);
-                }
+            } else {
+               char nextKey = (char) (current + shift);
+               encoded.append(nextKey);
             }
-        }
-        return encoded.toString();
-    }
-
-    /**
-     * Decode string.
-     *
-     * @param message the message
-     * @param shift   the shift
-     * @return the string
-     */
-    public static String decode(String message, int shift) {
-        StringBuilder decoded = new StringBuilder();
-        for (int i = 0; i < message.length(); i++) {
-            int current = message.charAt(i);
-            if (current == 32) {
-                decoded.append(" ");
-            } else if (current >= 65 && current <= 90) {
-                int numAlphabet = message.charAt(i);
-                if (numAlphabet - shift < 65) {
-                    int j = numAlphabet - 65;
-                    char nextKey = (char) (90 - (shift - j - 1));
-                    decoded.append(nextKey);
-
-                } else {
-                    char nextKey = (char) (current - shift);
-                    decoded.append(nextKey);
-                }
-            } else if (current >= 97 && current <= 122) {
-                int numAlphabet = message.charAt(i);
-                if (numAlphabet - shift < 97) {
-                    int j = numAlphabet - 97;
-                    char nextKey = (char) (122 - (shift - j - 1));
-                    decoded.append(nextKey);
-                } else {
-                    char nextKey = (char) (current - shift);
-                    decoded.append(nextKey);
-                }
+         } else if (current >= 97 && current <= 122) {
+            int numAlphabet = message.charAt(i);
+            if (shift + numAlphabet > 122) {
+               int j = 122 - numAlphabet;
+               char nextKey = (char) (97 + (shift - j - 1));
+               encoded.append(nextKey);
+            } else {
+               char nextKey = (char) (current + shift);
+               encoded.append(nextKey);
             }
-        }
-        return decoded.toString();
-    }
+         }
+      }
+      return encoded.toString();
+   }
 
-    /*public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the message (Latin Alphabet)");
-        String message = input.nextLine();
-        System.out.println(message);
-        System.out.println("Please enter the shift number");
-        int shift = input.nextInt() % 26;
-        System.out.println("(E)ncode or (D)ecode ?");
-        char choice = input.next().charAt(0);
-        if (choice == 'E' || choice == 'e')
-            System.out.println("ENCODED MESSAGE IS \n" + encode(message, shift)); //send our function to handle
-        if (choice == 'D' || choice == 'd')
-            System.out.println("DECODED MESSAGE IS \n" + decode(message, shift));
-    }*/
+   /**
+    * Decode string.
+    *
+    * @param message the message
+    * @param shift   the shift
+    * @return the string
+    */
+   public static String decode(String message, int shift) {
+      StringBuilder decoded = new StringBuilder();
+      for (int i = 0; i < message.length(); i++) {
+         int current = message.charAt(i);
+         if (current == 32) {
+            decoded.append(" ");
+         } else if (current >= 65 && current <= 90) {
+            int numAlphabet = message.charAt(i);
+            if (numAlphabet - shift < 65) {
+               int j = numAlphabet - 65;
+               char nextKey = (char) (90 - (shift - j - 1));
+               decoded.append(nextKey);
 
+            } else {
+               char nextKey = (char) (current - shift);
+               decoded.append(nextKey);
+            }
+         } else if (current >= 97 && current <= 122) {
+            int numAlphabet = message.charAt(i);
+            if (numAlphabet - shift < 97) {
+               int j = numAlphabet - 97;
+               char nextKey = (char) (122 - (shift - j - 1));
+               decoded.append(nextKey);
+            } else {
+               char nextKey = (char) (current - shift);
+               decoded.append(nextKey);
+            }
+         }
+      }
+      return decoded.toString();
+   }
 }
--- ciphers/src/main/java/com/github/chandanv89/java/ciphers/ColumnarTranspositionCipher.java
+++ ciphers/src/main/java/com/github/chandanv89/java/ciphers/ColumnarTranspositionCipher.java
@@ -6,174 +6,174 @@ package com.github.chandanv89.java.ciphers;
  * @author <a href="https://github.com/freitzzz">freitzzz</a>
  */
 public class ColumnarTranspositionCipher {
-    /**
-     * The constant ABECEDARIUM.
-     */
-    private static final String ABECEDARIUM = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.;:-@";
-    private static final String ENCRYPTION_FIELD = "≈";
+   /**
+    * The constant ABECEDARIUM.
+    */
+   private static final String ABECEDARIUM = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.;:-@";
+   private static final String ENCRYPTION_FIELD = "≈";
    private static final String ENCRYPTION_FIELD_CHAR = "≈";
-    private static String keyword;
-    private static Object[][] table;
-    private static String abecedarium;
+   private static String keyword;
+   private static Object[][] table;
+   private static String abecedarium;
 
-    /**
-     * Encrypts a certain String with the Columnar Transposition Cipher Rule
-     *
-     * @param word    Word being encrypted
-     * @param keyword String with keyword being used
-     * @return a String with the word encrypted by the Columnar Transposition Cipher Rule
-     */
-    public static String encrpyter(String word, String keyword) {
-        ColumnarTranspositionCipher.keyword = keyword;
-        abecedariumBuilder(500);
-        table = tableBuilder(word);
-        Object[][] sortedTable = sortTable(table);
-        StringBuilder wordEncrypted = new StringBuilder();
-        for (int i = 0; i < sortedTable[0].length; i++) {
-            for (int j = 1; j < sortedTable.length; j++) {
-                wordEncrypted.append(sortedTable[j][i]);
+   /**
+    * Encrypts a certain String with the Columnar Transposition Cipher Rule
+    *
+    * @param word    Word being encrypted
+    * @param keyword String with keyword being used
+    * @return a String with the word encrypted by the Columnar Transposition Cipher Rule
+    */
+   public static String encrpyter(String word, String keyword) {
+      ColumnarTranspositionCipher.keyword = keyword;
+      abecedariumBuilder(500);
+      table = tableBuilder(word);
+      Object[][] sortedTable = sortTable(table);
+      StringBuilder wordEncrypted = new StringBuilder();
+      for (int i = 0; i < sortedTable[0].length; i++) {
+         for (int j = 1; j < sortedTable.length; j++) {
+            wordEncrypted.append(sortedTable[j][i]);
+         }
+      }
+      return wordEncrypted.toString();
+   }
+
+   /**
+    * Encrypts a certain String with the Columnar Transposition Cipher Rule
+    *
+    * @param word        Word being encrypted
+    * @param keyword     String with keyword being used
+    * @param abecedarium String with the abecedarium being used. null for default one
+    * @return a String with the word encrypted by the Columnar Transposition Cipher Rule
+    */
+   public static String encrpyter(String word, String keyword, String abecedarium) {
+      ColumnarTranspositionCipher.keyword = keyword;
+      if (abecedarium != null) {
+         ColumnarTranspositionCipher.abecedarium = abecedarium;
+      } else {
+         ColumnarTranspositionCipher.abecedarium = ABECEDARIUM;
+      }
+      table = tableBuilder(word);
+      Object[][] sortedTable = sortTable(table);
+      String wordEncrypted = "";
+      for (int i = 0; i < sortedTable[0].length; i++) {
+         for (int j = 1; j < sortedTable.length; j++) {
+            wordEncrypted += sortedTable[j][i];
+         }
+      }
+      return wordEncrypted;
+   }
+
+   /**
+    * Decryps a certain encrypted String with the Columnar Transposition Cipher Rule
+    *
+    * @return a String decrypted with the word ecrypted by the Columnar Transpositiion Cipher Rule
+    */
+   public static String decrypter() {
+      String wordDecrypted = "";
+      for (int i = 1; i < table.length; i++) {
+         for (int j = 0; j < table[i].length; j++) {
+            wordDecrypted += table[i][j];
+         }
+      }
+      return wordDecrypted.replaceAll(ENCRYPTION_FIELD, "");
+   }
+
+   /**
+    * Builds a table with the word to be encrpyted in rows by the Columnar Transposition Cipher Rule
+    *
+    * @return An Object[][] with the word to be encrypted filled in rows and columns
+    */
+   private static Object[][] tableBuilder(String word) {
+      Object[][] table = new Object[rows(word) + 1][keyword.length()];
+      char[] wordInChards = word.toCharArray();
+      //Fils in the respective numbers
+      table[0] = findElements();
+      int charElement = 0;
+      for (int i = 1; i < table.length; i++) {
+         for (int j = 0; j < table[i].length; j++) {
+            if (charElement < wordInChards.length) {
+               table[i][j] = (char) wordInChards[charElement];
+               charElement++;
+            } else {
+               table[i][j] = ENCRYPTION_FIELD_CHAR;
             }
-        }
-        return wordEncrypted.toString();
-    }
+         }
+      }
+      return table;
+   }
 
-    /**
-     * Encrypts a certain String with the Columnar Transposition Cipher Rule
-     *
-     * @param word        Word being encrypted
-     * @param keyword     String with keyword being used
-     * @param abecedarium String with the abecedarium being used. null for default one
-     * @return a String with the word encrypted by the Columnar Transposition Cipher Rule
-     */
-    public static String encrpyter(String word, String keyword, String abecedarium) {
-        ColumnarTranspositionCipher.keyword = keyword;
-        if (abecedarium != null) {
-            ColumnarTranspositionCipher.abecedarium = abecedarium;
-        } else {
-            ColumnarTranspositionCipher.abecedarium = ABECEDARIUM;
-        }
-        table = tableBuilder(word);
-        Object[][] sortedTable = sortTable(table);
-        String wordEncrypted = "";
-        for (int i = 0; i < sortedTable[0].length; i++) {
-            for (int j = 1; j < sortedTable.length; j++) {
-                wordEncrypted += sortedTable[j][i];
+   /**
+    * Determines the number of rows the table should have regarding the Columnar Transposition Cipher Rule
+    *
+    * @return an int with the number of rows that the table should have in order to respect the Columnar Transposition Cipher Rule.
+    */
+   private static int rows(String word) {
+      if ((double) word.length() / keyword.length() > word.length() / keyword.length()) {
+         return (word.length() / keyword.length()) + 1;
+      } else {
+         return word.length() / keyword.length();
+      }
+   }
+
+   private static Object[] findElements() {
+      Object[] charValues = new Object[keyword.length()];
+      for (int i = 0; i < charValues.length; i++) {
+         for (int j = 0; j < abecedarium.length(); j++) {
+            if (keyword.charAt(i) == abecedarium.charAt(j)) charValues[i] = j;
+         }
+      }
+      return charValues;
+   }
+
+   private static Object[][] sortTable(Object[][] table) {
+      Object[][] tableSorted = new Object[table.length][table[0].length];
+      for (int i = 0; i < tableSorted.length; i++) {
+         System.arraycopy(table[i], 0, tableSorted[i], 0, tableSorted[i].length);
+      }
+      for (int i = 0; i < tableSorted[0].length; i++) {
+         for (int j = i + 1; j < tableSorted[0].length; j++) {
+            if (Integer.parseInt(String.valueOf(tableSorted[0][i])) > Integer.parseInt(String.valueOf(table[0][j]))) {
+               Object[] column = getColumn(tableSorted, tableSorted.length, i);
+               switchColumns(tableSorted, j, i, column);
             }
-        }
-        return wordEncrypted;
-    }
+         }
+      }
+      return tableSorted;
+   }
 
-    /**
-     * Decryps a certain encrypted String with the Columnar Transposition Cipher Rule
-     *
-     * @return a String decrypted with the word ecrypted by the Columnar Transpositiion Cipher Rule
-     */
-    public static String decrypter() {
-        String wordDecrypted = "";
-        for (int i = 1; i < table.length; i++) {
-            for (int j = 0; j < table[i].length; j++) {
-                wordDecrypted += table[i][j];
-            }
-        }
-        return wordDecrypted.replaceAll(ENCRYPTION_FIELD, "");
-    }
+   private static Object[] getColumn(Object[][] table, int rows, int column) {
+      Object[] columnArray = new Object[rows];
+      for (int i = 0; i < rows; i++) {
+         columnArray[i] = table[i][column];
+      }
+      return columnArray;
+   }
 
-    /**
-     * Builds a table with the word to be encrpyted in rows by the Columnar Transposition Cipher Rule
-     *
-     * @return An Object[][] with the word to be encrypted filled in rows and columns
-     */
-    private static Object[][] tableBuilder(String word) {
-        Object[][] table = new Object[rows(word) + 1][keyword.length()];
-        char[] wordInChards = word.toCharArray();
-        //Fils in the respective numbers
-        table[0] = findElements();
-        int charElement = 0;
-        for (int i = 1; i < table.length; i++) {
-            for (int j = 0; j < table[i].length; j++) {
-                if (charElement < wordInChards.length) {
-                    table[i][j] = (char) wordInChards[charElement];
-                    charElement++;
-                } else {
-                    table[i][j] = ENCRYPTION_FIELD_CHAR;
-                }
-            }
-        }
-        return table;
-    }
+   private static void switchColumns(Object[][] table, int firstColumnIndex, int secondColumnIndex, Object[] columnToSwitch) {
+      for (int i = 0; i < table.length; i++) {
+         table[i][secondColumnIndex] = table[i][firstColumnIndex];
+         table[i][firstColumnIndex] = columnToSwitch[i];
+      }
+   }
 
-    /**
-     * Determines the number of rows the table should have regarding the Columnar Transposition Cipher Rule
-     *
-     * @return an int with the number of rows that the table should have in order to respect the Columnar Transposition Cipher Rule.
-     */
-    private static int rows(String word) {
-        if ((double) word.length() / keyword.length() > word.length() / keyword.length()) {
-            return (word.length() / keyword.length()) + 1;
-        } else {
-            return word.length() / keyword.length();
-        }
-    }
+   /**
+    * Creates an abecedarium with a specified ascii inded
+    *
+    * @param value Number of characters being used based on the ASCII Table
+    */
+   private static void abecedariumBuilder(int value) {
+      abecedarium = "";
+      for (int i = 0; i < value; i++) {
+         abecedarium += (char) i;
+      }
+   }
 
-    private static Object[] findElements() {
-        Object[] charValues = new Object[keyword.length()];
-        for (int i = 0; i < charValues.length; i++) {
-            for (int j = 0; j < abecedarium.length(); j++) {
-                if (keyword.charAt(i) == abecedarium.charAt(j)) charValues[i] = j;
-            }
-        }
-        return charValues;
-    }
-
-    private static Object[][] sortTable(Object[][] table) {
-        Object[][] tableSorted = new Object[table.length][table[0].length];
-        for (int i = 0; i < tableSorted.length; i++) {
-            System.arraycopy(table[i], 0, tableSorted[i], 0, tableSorted[i].length);
-        }
-        for (int i = 0; i < tableSorted[0].length; i++) {
-            for (int j = i + 1; j < tableSorted[0].length; j++) {
-                if (Integer.parseInt(String.valueOf(tableSorted[0][i])) > Integer.parseInt(String.valueOf(table[0][j]))) {
-                    Object[] column = getColumn(tableSorted, tableSorted.length, i);
-                    switchColumns(tableSorted, j, i, column);
-                }
-            }
-        }
-        return tableSorted;
-    }
-
-    private static Object[] getColumn(Object[][] table, int rows, int column) {
-        Object[] columnArray = new Object[rows];
-        for (int i = 0; i < rows; i++) {
-            columnArray[i] = table[i][column];
-        }
-        return columnArray;
-    }
-
-    private static void switchColumns(Object[][] table, int firstColumnIndex, int secondColumnIndex, Object[] columnToSwitch) {
-        for (int i = 0; i < table.length; i++) {
-            table[i][secondColumnIndex] = table[i][firstColumnIndex];
-            table[i][firstColumnIndex] = columnToSwitch[i];
-        }
-    }
-
-    /**
-     * Creates an abecedarium with a specified ascii inded
-     *
-     * @param value Number of characters being used based on the ASCII Table
-     */
-    private static void abecedariumBuilder(int value) {
-        abecedarium = "";
-        for (int i = 0; i < value; i++) {
-            abecedarium += (char) i;
-        }
-    }
-
-    private static void showTable() {
-        for (Object[] aTable : table) {
-            for (Object anATable : aTable) {
-                System.out.print(anATable + " ");
-            }
-            System.out.println();
-        }
-    }
+   private static void showTable() {
+      for (Object[] aTable : table) {
+         for (Object anATable : aTable) {
+            System.out.print(anATable + " ");
+         }
+         System.out.println();
+      }
+   }
 }
